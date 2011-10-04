89491df16eec579b5c164166cc5e1730943ee543
Richard Benson <richard.benson@dixcart.com>
2011-10-04 16:07:28 +0100

    Settings include

---
+++ includes/settings.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+DEFINE('DB_SERVER', 'localhost');
+DEFINE('DB_USER', 'lb-logreader');
+DEFINE('DB_PASS', 'wibble');
+DEFINE('DB_NAME', 'lblogs');
+
+DEFINE('LOG_PATH', "/var/logs/whatever");
+
+?>
