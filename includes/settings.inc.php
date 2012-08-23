0dd1dca4d868315ac7b7a34fa5c95f12a0abecaa
Richard Benson <richard.benson@dixcart.com>
2012-08-23 18:07:52 +0100

    Download new logs automatically from Cloud Files

--- includes/settings.inc.php
+++ includes/settings.inc.php
@@ -1,10 +1,24 @@
 <?php
+//Enables more verbose messages
+DEFINE('DEBUG', false);
 
+//DB Settings
 DEFINE('DB_SERVER', 'localhost');
 DEFINE('DB_USER', 'lb-logreader');
 DEFINE('DB_PASS', 'wibble');
-DEFINE('DB_NAME', 'lblogs');
+DEFINE('DB_NAME', 'lb-logreader');
 
-DEFINE('LOG_PATH', "/var/logs/whatever");
+//Cloud Files authentication
+DEFINE('CF_USER', 'whatever');
+DEFINE('CF_KEY', 'yourapikeynotmine');
 
+//Path to store and analyse log files, if Windows use \\ or /
+DEFINE('LOG_PATH', "/the/path/to/wherever");
+
+//Set to true to remove from CF when downloaded
+DEFINE('DELETE_WHEN_DL', true);
+DEFINE('DELETE_EMPTY_CONT', true);
+
+//If set to true, will delete the file, if false will archive in subfolder of logs folder
+DEFINE('DELETE_WHEN_PROCESSED', false);
 ?>
